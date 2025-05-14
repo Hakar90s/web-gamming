e7f10c14dbe597339a106645652cfa132023d375
Hakar90s <hakar.github25@gmail.com>
2025-05-14 16:28:53 +0300

    Update level_data.py

--- level_data.py
+++ level_data.py
@@ -1,6 +1,6 @@
 level_data = {
     1: {
-        "image_url": "https://your-question-image-link.com",
+        "https:// raw.githubusercontent.com/Hakar90s/web-gamming/blob/main/images/level%201.jpg?raw=true.png",
         "answer_image_url": "https://your-answer-image-link.com",  # Replace this with the image you want to show after solving
         "question": "How many red apples do you see?",
         "answer": "3"
