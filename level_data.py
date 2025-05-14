aa856fdfbdfac042abcced1246c9cd456c93ac88
Hakar90s <hakar.github25@gmail.com>
2025-05-14 16:51:55 +0300

    Update level_data.py

--- level_data.py
+++ level_data.py
@@ -2,14 +2,13 @@ level_data = {
     1: {
         "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201%20A.png?raw=true",
-        "question": "what fruit is in the basket?",
-        "answer": "apple"
+        "question": "How many red apples do you see?",
+        "answer": "3"
     },
     2: {
-        "image_url": "https://your-question-image-link.com",
+        "image_url": "https://your-image-link.com",
         "answer_image_url": "https://your-answer-image-link.com",
         "question": "Which animal is shown in the image?",
         "answer": "elephant"
-    },
-    # Add more levels here...
+    }
 }
