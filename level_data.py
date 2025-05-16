e883c6cf7130854674c68919256f3ee681464eb4
Hakar90s <hakar.github25@gmail.com>
2025-05-16 11:06:24 +0300

    Update level_data.py

--- level_data.py
+++ level_data.py
@@ -2,12 +2,13 @@ level_data = {
     1: {
         "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201%20A.png?raw=true",
-        "question": "what fruit is in the basket?",
+        "question": "What fruit is in the basket?",
         "answer": "apple"
     },
     2: {
-        "image_url": "https:// raw.githubusercontent.com /Hakar90s/web-gamming/main/images/level%202.jpg?raw=true",
+        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202%20A.jpg?raw=true",
         "question": "Who won the Ballon d'Or 2014?",
         "answer": "ronaldo"
     }
+}
