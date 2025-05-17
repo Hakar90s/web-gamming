d8d8ba41bcca75947db0c32085bd2cf928a00c46
Hakar90s <hakar.github25@gmail.com>
2025-05-17 21:57:41 +0300

    Update level_data.py

--- level_data.py
+++ level_data.py
@@ -1,14 +1,34 @@
 level_data = {
-    1: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201.jpg?raw=true",
-        "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201%20A.png?raw=true",
-        "question": "What fruit is in the basket?",
-        "answer": "apple"
-    },
-    2: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202.jpg?raw=true",
-        "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202%20A.jpg?raw=true",
-        "question": "Who won the Ballon d'Or 2014?",
-        "answer": "ronaldo"
-    }
+    1:  {"image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201.jpg?raw=true",
+ "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201%20A.png?raw=true",
+ "question": "What fruit is in the basket?",
+ "answer": "apple"
+        },
+    2:  {"image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202.jpg?raw=true",
+ "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202%20A.jpg?raw=true",
+ "question": "Who won the Ballon d'Or 2014?",
+ "answer": "ronaldo"
+},
+    3:  {"image_url": "",
+ "answer_image_url": "",
+ "question": "",
+ "answer": ""
+},
+    4:  {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    5:  {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    6:  {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    7:  {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    8:  {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    9:  {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    10: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    11: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    12: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    13: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    14: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    15: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    16: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    17: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    18: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    19: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
+    20: {"image_url": "", "answer_image_url": "", "question": "", "answer": ""},
 }
