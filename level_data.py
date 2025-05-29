aaff56ad3e2da5ea32a9a803c1b017ebf1cd623c
Hakar90s <hakar.github25@gmail.com>
2025-05-29 23:20:31 +0300

    Update level_data.py

--- level_data.py
+++ level_data.py
@@ -2,95 +2,93 @@
 
 level_data = {
     1: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%201%20A.png?raw=true",
-        "question": "What fruit is in the basket?",
-        "answer": "apple"
+        "question":         "What fruit is in the basket?",
+        "answer":           "apple"
     },
     2: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%202%20A.jpg?raw=true",
-        "question": "Who won the Ballon d'Or 2014?",
-        "answer": "ronaldo"
+        "question":         "Who won the Ballon d'Or 2014?",
+        "answer":           "ronaldo"
     },
     3: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%203.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%203.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%203%20A.jpg?raw=true",
-        "question": "Choose the right answer",
-        "answer": "b"
+        "question":         "Choose the right answer",
+        "answer":           "b"
     },
     4: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%204.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%204.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%204%20A.jpg?raw=true",
-        "question": "Guess the right answer",
-        "answer": "a"
+        "question":         "Guess the right answer",
+        "answer":           "a"
     },
     5: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%205.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%205.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%205%20A.jpg?raw=true",
-        "question": "How is Alice related to David?",
-        "answer": "c"
+        "question":         "How is Alice related to David?",
+        "answer":           "c"
     },
     6: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%206.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%206.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%206%20A.jpg?raw=true",
-        "question": "How is Sarah related to Daniel?",
-        "answer": "b"
+        "question":         "How is Sarah related to Daniel?",
+        "answer":           "b"
     },
-       7: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%207.jpg?raw=true",
+    7: {
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%207.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%207%20A.jpg?raw=true",
-        "question": "What number comes next in the sequence?",
-        "answer": "c"
-    },
-
+        "question":         "What number comes next in the sequence?",
+        "answer":           "c"
     },
     8: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%208.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%208.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%208%20A.jpg?raw=true",
-        "question": "What number am I?",
-        "answer": "c"
+        "question":         "What number am I?",
+        "answer":           "c"
     },
     9: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%209.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%209.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/images/level%209%20A.jpg?raw=true",
-        "question": "What will happen when the ice melts?",
-        "answer": "b"
+        "question":         "What will happen when the ice melts?",
+        "answer":           "b"
     },
     10: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2010.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2010.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2010%20A.jpg?raw=true",
-        "question": "Which planet in our solar system has the most moons?",
-        "answer": "a"
+        "question":         "Which planet in our solar system has the most moons?",
+        "answer":           "a"
     },
     11: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2011.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2011.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2011%20A.jpg?raw=true",
-        "question": "Which capital city is the highest above sea level?",
-        "answer": "a"
+        "question":         "Which capital city is the highest above sea level?",
+        "answer":           "a"
     },
     12: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2012.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2012.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2012%20A.jpg?raw=true",
-        "question": "How many months have 28 days?",
-        "answer": "c"
+        "question":         "How many months have 28 days?",
+        "answer":           "c"
     },
     13: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2013.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2013.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2013%20A.png?raw=true",
-        "question": "Write the final result only in number",
-        "answer": "82"
+        "question":         "Write the final result only in number",
+        "answer":           "82"
     },
     14: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2014.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2014.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2014%20A.jpg?raw=true",
-        "question": "Write the right answer",
-        "answer": ["coin", "a coin"]
+        "question":         "Write the right answer",
+        "answer":           ["coin", "a coin"]
     },
     15: {
-        "image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2015.jpg?raw=true",
+        "image_url":        "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2015.jpg?raw=true",
         "answer_image_url": "https://raw.githubusercontent.com/Hakar90s/web-gamming/main/img/level%2015%20A.jpg?raw=true",
-        "question": "Only write the right number",
-        "answer": "11"
+        "question":         "Only write the right number",
+        "answer":           "11"
     }
 }
